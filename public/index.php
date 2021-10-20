c4bd74c096085a54e7d9179a0ecfa68dbc45e79c
MatthieuChabot <chbt.matthieu@gmail.com>
2021-10-20 16:09:28 +0200

    finishing quest

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-// Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
-$name = 'Wilder';
+$name = 'Matthieu';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
